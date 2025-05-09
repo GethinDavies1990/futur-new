4ac62fc651aa478318fa31ccd833177cc45ab110
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2025-05-09 19:59:59 +0100

    update content section

--- src/ui/modules/ContentSection.tsx
+++ src/ui/modules/ContentSection.tsx
@@ -1,11 +1,8 @@
 import { PortableText } from 'next-sanity'
 import Pretitle from '@/ui/Pretitle'
 import CTAList from '@/ui/CTAList'
-import Asset from './Asset'
 import CustomHTML from './CustomHTML'
 import Reputation from '@/ui/Reputation'
-import { RxShadow } from 'react-icons/rx'
-import { cn } from '@/lib/utils'
 
 export default function ContentSection({
 	pretitle,
@@ -17,11 +14,10 @@ export default function ContentSection({
 	ctas: Sanity.CTA[]
 }>) {
 	return (
-		<section className="section headings:text-black space-y-8 text-gray-600">
+		<section className="section headings:text-white space-y-8 text-gray-300">
 			<header className="richtext grid items-start justify-between gap-8 md:grid-cols-2 md:gap-x-12">
-				<div className="flex items-center justify-start">
-					<RxShadow className="text-accent mr-2" size={20} />
-					<Pretitle className="text-gray-600">{pretitle}</Pretitle>
+				<div className="flex max-w-lg items-center justify-start">
+					<Pretitle className="h2 text-gray-300">{pretitle}</Pretitle>
 				</div>
 				<div className="richtext headings:text-balance mx-auto w-full max-w-lg">
 					<PortableText
