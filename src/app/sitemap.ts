b14a9e3559666eed02373270209b2f0ba1bb5d87
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2025-05-21 18:13:52 +0100

    update colour

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,37 +8,41 @@ import type { MetadataRoute } from 'next'
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 	const data = await fetchSanityLive<Record<string, MetadataRoute.Sitemap>>({
 		query: groq`{
-			'pages': *[
-				_type == 'page' &&
-				!(metadata.slug.current in ['404']) &&
-				metadata.noIndex != true
-			]|order(metadata.slug.current){
-				'url': (
-					$baseUrl
-					+ select(defined(language) && language != $defaultLang => language + '/', '')
-					+ select(
-						metadata.slug.current == 'index' => '',
-						metadata.slug.current
-					)
-				),
-				'lastModified': _updatedAt,
-				'priority': select(
-					metadata.slug.current == 'index' => 1,
-					0.5
-				),
-			},
-			'blog': *[_type == 'blog.post' && metadata.noIndex != true]|order(name){
-				'url': (
-					$baseUrl
-					+ select(defined(language) && language != $defaultLang => language + '/', '')
-					+ '${BLOG_DIR}/'
-					+ '${WORK_DIR}/'
-					+ metadata.slug.current
-				),
-				'lastModified': _updatedAt,
-				'priority': 0.4
-			}
-		}`,
+  // Blog posts
+  'blog': *[_type == 'blog.post' && metadata.noIndex != true] | order(name) {
+    'url': (
+      $baseUrl
+      + select(defined(language) && language != $defaultLang => language + '/', '')
+      + '${BLOG_DIR}/'
+      + metadata.slug.current
+    ),
+    'lastModified': _updatedAt,
+    'priority': 0.4
+  },
+
+  // Work posts
+  'work': *[_type == 'work.post' && metadata.noIndex != true] | order(name) {
+    'url': (
+      $baseUrl
+      + select(defined(language) && language != $defaultLang => language + '/', '')
+      + '${WORK_DIR}/'
+      + metadata.slug.current
+    ),
+    'lastModified': _updatedAt,
+    'priority': 0.4
+  },
+
+  // Optional: Static pages
+  'pages': *[_type == 'page' && metadata.noIndex != true] | order(name) {
+    'url': (
+      $baseUrl
+      + select(defined(language) && language != $defaultLang => language + '/', '')
+      + metadata.slug.current
+    ),
+    'lastModified': _updatedAt,
+    'priority': 0.6
+  }
+}`,
 		params: {
 			baseUrl: process.env.NEXT_PUBLIC_BASE_URL + '/',
 			defaultLang: DEFAULT_LANG,
